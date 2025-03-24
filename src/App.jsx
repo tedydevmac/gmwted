4dbb55320384cf1465f3ae0e966ec733f32d7d3e
Goh Min Wen, Ted <goh_min_wen_ted@s2022.ssts.edu.sg>
2025-03-24 17:19:49 +0800

    changed path names

--- src/App.jsx
+++ src/App.jsx
@@ -81,7 +81,7 @@ export const App = () => {
               </span>
             </p>
           </div>
-          <img className="profile" src="/static/logos/IMG_3817.png" />
+          <img className="profile" src="/assets/logos/IMG_3817.png" />
         </div>
         <Skills />
         <div className="content" id="projects">
@@ -92,14 +92,14 @@ export const App = () => {
           <div className="column">
             <Card
               images={[
-                "/static/OH2024/OHMain.PNG",
-                "/static/OH2024/OHAndroid.jpeg",
-                "/static/OH2024/OHIOS.PNG",
+                "/assets/OH2024/OHMain.PNG",
+                "/assets/OH2024/OHAndroid.jpeg",
+                "/assets/OH2024/OHIOS.PNG",
               ]}
               title="SST Open House App 2024 (IOS/Android)"
               description="I am one of the lead developers for this app which was used during SST's Open House event."
               imageUrl="fa-solid fa-door-open"
-              imageSrc={"/static/logos/SSTOH2024.png"}
+              imageSrc={"/assets/logos/SSTOH2024.png"}
               link={
                 "https://apps.apple.com/us/app/sst-open-house-2024/id6479939416"
               }
@@ -114,11 +114,11 @@ export const App = () => {
             <Card
               tooBig={false}
               singleImage={false}
-              images={["/static/logos/ASPA.png"]}
+              images={["/assets/logos/ASPA.png"]}
               title="ASPA (IOS/Android)"
               description="This was my 2023 Serve project which I worked on until 2024"
               imageUrl="fa-solid fa-user-doctor"
-              imageSrc={"/static/logos/ASPA.png"}
+              imageSrc={"/assets/logos/ASPA.png"}
               github={"https://github.com/tedydevmac/aspa.git"}
               iconHover={false}
               expanddesc={
@@ -128,11 +128,11 @@ export const App = () => {
             <Card
               tooBig={false}
               singleImage={false}
-              images={["/static/logos/virtualtour.png"]}
+              images={["/assets/logos/virtualtour.png"]}
               title="SST Virtual Tour (Web)"
               description="This was my 2024 Serve project which I am still currently working on"
               imageUrl="fa-solid fa-school"
-              imageSrc={"/static/logos/virtualtour.png"}
+              imageSrc={"/assets/logos/virtualtour.png"}
               github={"https://github.com/tedydevmac/virtualtour.git"}
               iconHover={false}
               expanddesc={
@@ -142,11 +142,11 @@ export const App = () => {
             <Card
               tooBig={false}
               singleImage={false}
-              images={["/static/logos/sstannouncer.png"]}
+              images={["/assets/logos/sstannouncer.png"]}
               title="SST Announcer (Android)"
               description="As a volunteer, I helped add new features to the app and improved its UI during its revamp in 2023"
               imageUrl="fa-solid fa-school"
-              imageSrc={"/static/logos/sstannouncer.png"}
+              imageSrc={"/assets/logos/sstannouncer.png"}
               link={
                 "https://play.google.com/store/apps/details?id=com.sst.anouncements&pcampaignid=web_share"
               }
@@ -159,11 +159,11 @@ export const App = () => {
             <Card
               tooBig={false}
               singleImage={false}
-              images={["/static/logos/sstinc.png"]}
+              images={["/assets/logos/sstinc.png"]}
               title="SST Inc Camp 2024 Hackathon"
               description="A 1 day hackathon where we built a mobile app to spice up the game of TicTacToe"
               imageUrl="fa-brands fa-telegram"
-              imageSrc={"/static/logos/sstinc.png"}
+              imageSrc={"/assets/logos/sstinc.png"}
               github={
                 "https://github.com/tedydevmac/sstinccamp2024hackathon.git"
               }
@@ -183,11 +183,11 @@ export const App = () => {
             <Card
               tooBig={false}
               singleImage={false}
-              images={["/static/logos/RAYdar.png"]}
+              images={["/assets/logos/RAYdar.png"]}
               title="SUTD What The Hack 2024"
               description="An AI enabled Chrome extension that tackles inequality regarding LGBTQ+ and minority groups"
               imageUrl="fa-brands fa-chrome"
-              imageSrc={"/static/logos/RAYdar.png"}
+              imageSrc={"/assets/logos/RAYdar.png"}
               link={"https://devpost.com/software/raydar-8dh1xi"}
               github={"https://github.com/tedydevmac/RAYdar.git"}
               iconHover={true}
@@ -201,15 +201,15 @@ export const App = () => {
                 "My first external CTF competition I joined alongside my friends"
               }
               imageUrl={"fa-solid fa-flag"}
-              imageSrc={"/static/logos/blahajctf.png"}
+              imageSrc={"/assets/logos/blahajctf.png"}
               iconHover={false}
               tooBig={false}
               singleImage={false}
               images={[
-                "/static/blahaj/blahaj1.png",
-                "/static/blahaj/blahaj2.png",
-                "/static/blahaj/blahaj3.png",
-                "/static/blahaj/blahaj4.png",
+                "/assets/blahaj/blahaj1.png",
+                "/assets/blahaj/blahaj2.png",
+                "/assets/blahaj/blahaj3.png",
+                "/assets/blahaj/blahaj4.png",
               ]}
               expanddesc={
                 "This was my first ever external competitive CTF competition that I have joined. \nI had done CTFs before in school hosted by SST Inc, but those had more beginner level questions. The questions I encountered were MUCH more difficult. \nLuckily, I had attended their training beforehand and I was able to solve a surpising amount of questions with the help of my teammates. Unfortuantely, despite our best efforts, we only managed to reach the top 30 teams and did not make it to the finals. However, I think it was still a good learning experience in the area of cybersecurity."
@@ -218,13 +218,13 @@ export const App = () => {
             <Card
               tooBig={false}
               singleImage={false}
-              images={["/static/logos/astrigues.png"]}
+              images={["/assets/logos/astrigues.png"]}
               title={"Raffles Astronomy Astrigue' 24 Nocturne"}
               description={
                 "Won 3rd place overall in the competition with my team"
               }
               imageUrl={"fa-solid fa-earth-europe"}
-              imageSrc={"/static/logos/astrigue.png"}
+              imageSrc={"/assets/logos/astrigue.png"}
               link={"https://github.com/tedydevmac/SSTSUTDBigD.git"}
               github={"https://github.com/tedydevmac/SSTSUTDBigD.git"}
               iconHover={false}
@@ -235,11 +235,11 @@ export const App = () => {
             <Card
               tooBig={false}
               singleImage={true}
-              images={["/static/logos/noi.jpeg"]}
+              images={["/assets/logos/noi.jpeg"]}
               title={"National Olympiad in Informatics (NOI) 2025"}
               description={"Obtained Participation. My first NOI competition."}
               imageUrl={"fa-solid fa-computer"}
-              imageSrc={"/static/logos/noi.jpeg"}
+              imageSrc={"/assets/logos/noi.jpeg"}
               link={"https://noisg.comp.nus.edu.sg/noi/"}
               iconHover={false}
               expanddesc={
@@ -249,13 +249,13 @@ export const App = () => {
             <Card
               tooBig={false}
               singleImage={true}
-              images={["/static/logos/sg60portal.png"]}
+              images={["/assets/logos/sg60portal.png"]}
               title={"SP National Software Competition 2025"}
               description={
                 "Won 1st place. A SG60 Portal app, all-in-one platform for SG60 related matters."
               }
               imageUrl={"fa-brands fa-app-store-ios"}
-              imageSrc={"/static/logos/sg60portal.png"}
+              imageSrc={"/assets/logos/sg60portal.png"}
               link={"https://devpost.com/software/sg60portal"}
               github={"https://github.com/tedydevmac/SP_NSComp.git"}
               iconHover={true}
@@ -266,13 +266,13 @@ export const App = () => {
             <Card
               tooBig={false}
               singleImage={true}
-              images={["/static/perse/persecc.png"]}
+              images={["/assets/perse/persecc.png"]}
               title={"Perse Coding Challenge 2023 - 2025"}
               description={
                 "Won Distinction - Best in School award in 2023, Won Distinction in 2025 "
               }
               imageUrl={"fa-solid fa-terminal"}
-              imageSrc={"/static/logos/persecc.jpg"}
+              imageSrc={"/assets/logos/persecc.jpg"}
               link={"https://pctc.perse.co.uk/"}
               iconHover={false}
               expanddesc={
@@ -282,13 +282,13 @@ export const App = () => {
             <Card
               tooBig={false}
               singleImage={false}
-              images={["/static/logos/sst.png"]}
+              images={["/assets/logos/sst.png"]}
               title={"SUTD-SST-NP Big-D Camp 2025"}
               description={
                 "A 3-day camp where we worked on building a remote controlled robot for post disaster debris management"
               }
               imageUrl={"fa-solid fa-robot"}
-              imageSrc={"/static/logos/sst.png"}
+              imageSrc={"/assets/logos/sst.png"}
               iconHover={false}
               github={"https://github.com/tedydevmac/SSTSUTDBigD.git"}
               expanddesc={
@@ -298,13 +298,13 @@ export const App = () => {
             <Card
               tooBig={false}
               singleImage={false}
-              images={["/static/logos/sst.png"]}
+              images={["/assets/logos/sst.png"]}
               title={"ARTC-SST Innovation Camp 2024"}
               description={
                 "An innovation camp where we worked on a IoT-enabled hydroponics system to boost plant growth. Won Most Environmentally Sustainable Award."
               }
               imageUrl={"fa-solid fa-gear"}
-              imageSrc={"/static/logos/sst.png"}
+              imageSrc={"/assets/logos/sst.png"}
               link={"https://github.com/tedydevmac/SSTSUTDBigD.git"}
               github={"https://github.com/tedydevmac/SSTSUTDBigD.git"}
               iconHover={false}
@@ -323,11 +323,11 @@ export const App = () => {
             <Card
               tooBig={false}
               singleImage={false}
-              images={["/static/logos/telegram.webp"]}
+              images={["/assets/logos/telegram.webp"]}
               title="Student Helper AI Bot - ARTC-SST Innovation Camp"
               description="A Telegram bot to assist students in their school work management"
               imageUrl="fa-brands fa-telegram"
-              imageSrc={"/static/logos/telegram.webp"}
+              imageSrc={"/assets/logos/telegram.webp"}
               github={"https://github.com/tedydevmac/studenthelp.git"}
               iconHover={false}
               expanddesc={
@@ -337,11 +337,11 @@ export const App = () => {
             <Card
               tooBig={false}
               singleImage={false}
-              images={["/static/logos/python.png"]}
+              images={["/assets/logos/python.png"]}
               title="Cyberbullying vs AI - S3 Computing+ Coursework "
               description="This project focuses on detecting and classifying cyberbullying in text, particularly against the LGBTQ community and minority groups."
               imageUrl="fa-solid fa-brain"
-              imageSrc={"/static/logos/python.png"}
+              imageSrc={"/assets/logos/python.png"}
               link={"https://github.com/tedydevmac/S3T3CPPT.git"}
               github={"https://github.com/tedydevmac/S3T3CPPT.git"}
               iconHover={false}
@@ -352,11 +352,11 @@ export const App = () => {
             <Card
               tooBig={false}
               singleImage={false}
-              images={["/static/logos/python.png"]}
+              images={["/assets/logos/python.png"]}
               title="Virtual Photobooth - S4 Computing+ Coursework "
               description="This project focuses on detecting and classifying cyberbullying in text, particularly against the LGBTQ community and minority groups."
               imageUrl="fa-solid fa-brain"
-              imageSrc={"/static/logos/python.png"}
+              imageSrc={"/assets/logos/python.png"}
               link={"https://github.com/tedydevmac/py-photobooth.git"}
               github={"https://github.com/tedydevmac/py-photobooth.git"}
               iconHover={false}
@@ -367,11 +367,11 @@ export const App = () => {
             <Card
               tooBig={false}
               singleImage={false}
-              images={["/static/logos/sst.png"]}
+              images={["/assets/logos/sst.png"]}
               title="WorkBud - S2 ChangeMakers Project"
               description="A self-help application to educate and teach SSTudents how to exercise properly by providing guides and instructions to execute certain common exercises based on muscle group or target group"
               imageUrl="fa-solid fa-dumbbell"
-              imageSrc={"/static/logos/sst.png"}
+              imageSrc={"/assets/logos/sst.png"}
               github={"https://github.com/tedydevmac/innofest-app.git"}
               iconHover={false}
               expanddesc={
